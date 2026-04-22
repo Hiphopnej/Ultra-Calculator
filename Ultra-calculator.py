9b6dda895ee6882300c667df0e7dbd3cb3638fbd
Alvar Eklind <alvar.eklind@gmail.com>
2026-04-22 08:49:34 +0200

    add pq solver and add it to the ultra calculator

--- Ultra-calculator.py
+++ Ultra-calculator.py
@@ -1,10 +1,13 @@
 from Calculator import calculator
 from Square_root_calculator import square_root
+from Pq_solver import pq_solver
 
-choices = ["calculator", "square root"]
+choices = ["calculator", "square root", "pq solver"]
 file_choice = input(f"What to you want to calulate. Options: {choices} ")
 
 if file_choice.lower() == "calculator":
     calculator(0,"",0,True,True)
 elif file_choice == "square root":
     square_root(0,0, True, True)
+elif file_choice == "pq solver":
+    pq_solver()
